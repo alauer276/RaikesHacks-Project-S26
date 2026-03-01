e493e4a8dd5c807903d3244bd8247d2ca34e4582
Anand Seemakurthy <anandseemakurthy@gmail.com>
2026-02-28 23:33:17 -0600

    Adding more filter values

--- Model/TicketSale.cs
+++ Model/TicketSale.cs
@@ -17,8 +17,11 @@ namespace RaikesHacks_Project_S26.Model
     {
         Football,
         Volleyball,
-        Basketball,
-        Music
+        M. Basketball,
+        W. Basketball,
+        Baseball,
+        Music,
+        Softball
     }
 
     public class TicketSale
